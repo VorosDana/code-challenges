28fcef990515032bded1808594da83e2544e782a
Dana Voros <vorosdana@gmail.com>
2019-04-08 14:59:50 -0700

    regex matching complete, refactored to recursive

--- es6/leetcode/regular-expression-matching/solution.js
+++ es6/leetcode/regular-expression-matching/solution.js
@@ -4,60 +4,52 @@
  * @return {boolean}
  */
 var isMatch = function (s, p) {
-  let output = false;
-  let str = s.split('');
-  let reg = p.split('');
+  // let output = false;
 
-  while (reg.length > 0) {
-    if (str.length === 0) {
-      return false;
+  if (s.length === 0) {
+    if (p.length === 0) {
+      return true;
     }
-    if (reg[1] === '*') {
-      while (reg[2] === reg[0]) {
-        reg.splice(2, 1);
-      }
-      if (reg[0] === '.') {
-        if (reg.length === 2) {
-          return true
-        }
-        if (reg[2] !== '.') {
-          if (str.includes(reg[2])) {
-            str = str.slice(str.indexOf(reg[2]))
-          } else {
-            if (str.length > 0) {
-              return false;
-            } else {
-              return true;
-            }
-          }
-        } else {
-          return false;
-        }
-      }
-      while (reg[0] === str[0]) {
-        str = str.slice(1);
-      }
-      reg = reg.slice(2);
-      continue;
+    if(p[1] === '*'){
+      return isMatch(s, p.slice(2));
     }
-    if (reg[0] === '.') {
-      str = str.slice(1);
-      reg = reg.slice(1);
-      continue;
-    }
-    if (reg[0] === str[0]) {
-      str = str.slice(1);
-      reg = reg.slice(1);
-    }
+    return false;
   }
-  return (str.length === 0);
+  if (p[1] === '*') {
+    let nextIndex = -1;
+    if (p[0] === '.') {
+      nextIndex = s.length;
+    } else {
+      for (let i = 0; i <= s.length; i++) {
+        nextIndex = i;
+        if (s[i] !== p[0]) {
+          break;
+        }
+      }
+    }
+    for (let i = 0; i <= nextIndex; i++) {
+      if (isMatch(s.slice(i), p.slice(2))) {
+        return true
+      }
+    }
+    return isMatch(s, p.slice(2));
+  }
+  if (p[0] === '.') {
+    return isMatch(s.slice(1), p.slice(1));
+  }
+  if (p[0] === s[0]) {
+    return isMatch(s.slice(1), p.slice(1));
+  }
+  // return (s.length === 0);
+  return false;
 };
 
-// console.log('Test 1:', isMatch('aa', 'a'), 'expected false');
-// console.log('Test 2:', isMatch('aa', 'a*'), 'expected true');
-// console.log('Test 3:', isMatch('ab', '.*'), 'expected true');
-// console.log('Test 4:', isMatch('aab', 'c*a*b'), 'expected true');
-// console.log('Test 5:', isMatch('mississippi', 'mis*is*p*.'), 'expected false');
-// console.log('Test 6:', isMatch('ab', '.*c'), 'expected false');
-// console.log('Test 7:', isMatch('aaa', 'a*a'), 'expected true');
+console.log('Test 1:', isMatch('aa', 'a'), 'expected false');
+console.log('Test 2:', isMatch('aa', 'a*'), 'expected true');
+console.log('Test 3:', isMatch('ab', '.*'), 'expected true');
+console.log('Test 4:', isMatch('aab', 'c*a*b'), 'expected true');
+console.log('Test 5:', isMatch('mississippi', 'mis*is*p*.'), 'expected false');
+console.log('Test 6:', isMatch('ab', '.*c'), 'expected false');
+console.log('Test 7:', isMatch('aaa', 'a*a'), 'expected true');
 console.log('Test 8:', isMatch('aaa', 'ab*a*c*a'), 'expected true');
+console.log('Test 9:', isMatch('a', 'ab*'), 'expected true');
